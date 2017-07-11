f0e6c72723927aa41268782de31a40c789891b72
Mark Paluch <mpaluch@pivotal.io>
2017-07-11 17:39:33 +0200

    demo

--- src/test/java/com/example/TestingWithSpringBootApplicationTests.java
+++ src/test/java/com/example/TestingWithSpringBootApplicationTests.java
@@ -16,18 +16,17 @@
 
 package com.example;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Integration test for the whole application.
@@ -36,13 +35,9 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@AutoConfigureTestDatabase
 public class TestingWithSpringBootApplicationTests {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @LocalServerPort
+    @Value("${local.server.port}")
     private int localServerPort;
 
     @Test
@@ -52,8 +47,11 @@ public class TestingWithSpringBootApplicationTests {
     @Test
     public void test() {
 
-        ResponseEntity<String> response = this.restTemplate.getForEntity("/cars/{name}", String.class, "Honda");
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:{port}/cars/{name}", String.class,
+                localServerPort, "Honda");
 
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).contains("Honda");
     }
 }
--- src/test/java/com/example/external/data/CarRepositoryIntegrationTests.java
+++ src/test/java/com/example/external/data/CarRepositoryIntegrationTests.java
@@ -22,13 +22,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Mark Paluch
  */
-@RunWith(SpringRunner.class)
-@DataJpaTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class CarRepositoryIntegrationTests {
 
     @Autowired
